990ea9d76094c4a6704c8d1772fc203e9c470a61
Werberus <rusyak777@list.ru>
2021-06-29 12:27:32 +0300

    Update 05_task_if_else.py

--- Module2/practice/if_else/05_task_if_else.py
+++ Module2/practice/if_else/05_task_if_else.py
@@ -3,4 +3,14 @@
 # Формат входных данных: дано целое число m (1 ≤ m ≤ 12).
 # Формат выходных данных: требуется вывести название времени года
 
-# TODO: your code here
+print("Введите номер месяца от 1 до 12:")
+month = int(input())
+
+if month == 12 or month == 1 or month == 2:
+    print("Зима")
+elif month == 3 or month == 4 or month == 5:
+    print("Весна")
+elif month == 6 or month == 7 or month == 8:
+    print("Лето")
+elif month == 9 or month == 10 or month == 11:
+    print("Осень")
