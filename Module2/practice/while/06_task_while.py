aa9e8120ede8189ac30cbb1c7fc86e0d9402e36a
Werberus <rusyak777@list.ru>
2021-06-29 15:39:04 +0300

    Update 06_task_while.py

--- Module2/practice/while/06_task_while.py
+++ Module2/practice/while/06_task_while.py
@@ -16,4 +16,22 @@
 # 220 284
 # 284 220
 
-# TODO: your code here
+num = int(input("Введите целое i: "))
+first_friend = 1
+
+while first_friend <= num:
+    second_friend = 1
+    sum1 = 0
+    while second_friend < first_friend:
+        if first_friend % second_friend == 0:
+            sum1 += second_friend
+        second_friend += 1
+    second_friend = 1
+    sum2 = 0
+    while second_friend < sum1:
+        if sum1 % second_friend == 0:
+            sum2 += second_friend
+        second_friend += 1
+    if sum2 == first_friend and sum1 == second_friend and first_friend != second_friend:
+        print(first_friend, second_friend)
+    first_friend += 1
