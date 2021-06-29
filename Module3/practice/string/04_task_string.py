977c2cff46b2fafa9d437c529c3b9644722c0c28
Werberus <rusyak777@list.ru>
2021-06-29 17:05:02 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,11 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
-# TODO: your code here
+i = 0
+arr_str = text.split(" ")
+count = 0
+while i < len(arr_str):
+    if len(arr_str[i]) > 5:
+        count += 1
+    i += 1
+print(count)
