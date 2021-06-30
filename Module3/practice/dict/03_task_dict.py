f03fbf77b3c5eba244a261e8f5b2600e211ac006
Werberus <rusyak777@list.ru>
2021-06-30 17:52:00 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -26,23 +26,56 @@ staff = [
         'salary': 47800
     },
 ]
-# Вычислите:
+
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
-# TODO: your code here
+max_sal = 0
+max_name = ""
+for a in staff:
+    if a.get("salary") > max_sal:
+        max_sal = a.get("salary")
+        max_name = a.get("name")
+print(max_name,"\n")
+
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
 
-# TODO: your code here
+min_sal = max_sal
+min_name = ""
+for a in staff:
+    if a.get("salary") < min_sal:
+        min_sal = a.get("salary")
+        min_name = a.get("name")
+print(min_name,"\n")
 
 print("Среднеарифметическую зарплату всех сотрудников")
 
-# TODO: your code here
+sal = 0
+for a in staff:
+    sal += a.get("salary")
+print(round(sal/len(staff), 2),"\n")
 
 print("Количество однофамильцев в организации")
 
-# TODO: your code here
+namer = 0
+for a in staff:
+    for b in staff:
+        if a != b:
+            if a.get("surname") == b.get("surname"):
+                namer +=1
+                break
+print(namer, "\n")
+
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
 
-# TODO: your code here
+
+for a in range(len(staff)):
+    i = 0
+    while i < len(staff)-1:
+        if staff[i].get("salary") > staff[i+1].get("salary"):
+            staff[i], staff[i+1] = staff[i+1], staff[i]
+        i += 1
+
+for a in staff:
+    print(f"{a.get('name')}, {a.get('surname')}, {a.get('salary')}")
