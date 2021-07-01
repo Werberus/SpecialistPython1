438aa3dff58c00fc9501a9584f511ed404a80a12
Werberus <rusyak777@list.ru>
2021-07-01 17:01:14 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -3,5 +3,12 @@
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
 def gen_list(size, of, to):
-    pass
+    li = []
+    for _ in range(size):
+        li.append(random.randint(of, to))
+    return li
 
+
+l = gen_list(5, 1, 100)
+
+print(l)
