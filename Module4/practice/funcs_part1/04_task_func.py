fb40e6189942d06894a6361d0713c551e9bea11f
Werberus <rusyak777@list.ru>
2021-07-01 12:39:22 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -1,12 +1,17 @@
-# Даны координаты трех точек p1(x1; y1) p2(x2; y2) p3(x3; y3).
-# Напишите функцию, проверябщую можно ли построить треугольник, соединив данные точки отрезками
+def distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
+
 
 def can_triangle(p1, p2, p3):
-    # TODO: your code here
-    pass
+    a = distance(p1, p2)
+    b = distance(p2, p3)
+    c = distance(p3, p1)
+    if (a + b > c) & (a + c > b) & (b + c > a):
+        return True
+    return False
 
 
 # Пример вызова функции
-can_triangle((10, 12), (14, 18), (12, 12))
-
-# Не забудьте протестировать вашу функцию
+print(can_triangle((14, 18), (14, 18), (12, 12)))
