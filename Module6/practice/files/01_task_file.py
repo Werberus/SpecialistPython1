17b810df8d6716cc18609fabbd37ec76d56c4ebf
Werberus <rusyak777@list.ru>
2021-07-02 15:28:17 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,13 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+with open("data/limericks.txt", "r", encoding="utf-8") as file:
+    count_char = 0
+    count_bit = 1
+    for f in file:
+        print(f.rstrip())
+        count_char += len(f.rstrip().replace(" ", ""))
+        if f.rstrip() == "":
+            count_bit += 1
+    print("\nколичество символов:", count_char)
+    print("количество стихотворений:", count_bit)
