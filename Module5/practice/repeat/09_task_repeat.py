2d453882f0412993dfacdb72a3a70c686e365650
Werberus <rusyak777@list.ru>
2021-07-02 12:35:18 +0300

    Update 09_task_repeat.py

--- Module5/practice/repeat/09_task_repeat.py
+++ Module5/practice/repeat/09_task_repeat.py
@@ -6,3 +6,23 @@
 # Результат: фруктов на букву “к” больше.
 # Дано: [“ананас”, “яблоко”, “Арбуз”, “киви”, “Клюква”, “банан”, “хурма”]
 # Результат: фруктов на букву “к”и “а” больше.
+def check_fruits(fruits):
+    count = 0
+    char = set()
+    for fruit in fruits:
+        count_new = 0
+        for fruit2 in fruits:
+            if not fruit2 == fruits:
+                if fruit.upper()[0] == fruit2.upper()[0]:
+                    count_new += 1
+        if count_new > count:
+            count = count_new
+            char = set()
+            char.add(fruit.upper()[0])
+        elif count_new == count:
+            char.add(fruit.upper()[0])
+    return char
+
+
+
+print(check_fruits(["ананас", "яокос", "Арбуз", "киви", "Клюква", "банан", "хурма"]))
