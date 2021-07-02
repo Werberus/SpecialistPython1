94dcda6d0d520415db44b98caab19c394a42a6b1
Werberus <rusyak777@list.ru>
2021-07-02 15:46:31 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,21 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+sum_price = 0
+max_price = 0
+with open("data/sold.txt", "r", encoding="utf-8") as file:
+    l =[]
+    for f in file:
+        l += (f.rstrip().split(" "))
+min_price = float(l[0])
+print(l)
+for i in l:
+    sum_price += float(i)
+    if float(i) > max_price:
+        max_price = float(i)
+    if float(i) < min_price:
+        min_price = float(i)
+
+print("На какую сумму было продано товаров:", sum_price)
+print("Цена самого дорогого товара:", max_price)
+print("Цена самого дешевого товара:", min_price)
