42bb9124de815ac24d0c17b888c33452aa574dae
Werberus <rusyak777@list.ru>
2021-07-02 11:20:28 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,7 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def last_pagination(num_items, items_on_page):
+    return num_items-(num_items//items_on_page*items_on_page)
+
+print(last_pagination(21,4))
